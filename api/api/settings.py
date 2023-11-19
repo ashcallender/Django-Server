595945ca2d099c549bfd83953b42b370f517862b
ashcallender <ashton.callender@selu.edu>
2023-11-19 15:13:55 -0600

    hostingtry2

--- api/api/settings.py
+++ api/api/settings.py
@@ -144,7 +144,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ORIGIN_ALLOW_ALL = False
 
